dac0584fabf14a08d9797f8c0914251f8d941e1b
William Andersson <wandersson.98@gmail.com>
2020-11-20 15:06:02 +0100

    json to xmi resource working, model improvements

--- eclipse-projects/tdt4250.project.resources/src/LoadResourceToXMI.java
+++ eclipse-projects/tdt4250.project.resources/src/LoadResourceToXMI.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -10,30 +12,23 @@ import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.emfjson.jackson.resource.JsonResourceFactory;
 
 import tdt4250.project.model.cocktail.*;
-import tdt4250.project.model.cocktail.util.*;
 
 
 public class LoadResourceToXMI {
 
 	public static void main(String[] args) throws IOException {
 		ResourceSet resourceSet = new ResourceSetImpl();
-		
 		resourceSet.getPackageRegistry().put(CocktailPackage.eNS_URI, CocktailPackage.eINSTANCE);
-
-		resourceSet.getResourceFactoryRegistry()
-		.getExtensionToFactoryMap()
-		.put("*", new JsonResourceFactory());
-		
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("json", new JsonResourceFactory());
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new  XMIResourceFactoryImpl());
 		
 		Resource resource = resourceSet.createResource(URI.createFileURI("resources/data.json"));
 		resource.load(null);
 		
 		CocktailParty cp = (CocktailParty) resource.getContents().get(0);
 		
-		System.out.println("CocktailParty: " + cp);
-		
-		for (Drink drink : cp.getDrinkRegister()) {
-			System.out.println(drink);
-		}
+		Resource xmiResource = resourceSet.createResource(URI.createFileURI("resources/CocktailParty.xmi"));
+		xmiResource.getContents().add(cp);
+		xmiResource.save(Collections.EMPTY_MAP);
 	}
 }
